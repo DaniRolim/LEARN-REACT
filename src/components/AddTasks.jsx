0d924fff467f842859aed234e278d764de0696bb
DaniRolim <113808614+DaniRolim@users.noreply.github.com>
2026-06-15 17:32:14 +0000

    adiciona funcionalidade de gerenciamento de tarefas com adição, exclusão e detalhes, além de atualizar dependências

--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,7 +1,38 @@
-function AddTasks() {
+import { useState } from "react";
+
+function AddTasks({onAddTaskSubmit}) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    console.log(onAddTaskSubmit);
     return (
-        <div>
-            <h1>Tast2</h1>
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow-md flex flex-col">
+            <input 
+                className="border-2 border-slate-300 outline-slate-400 px-4 py-2 rounded-md" 
+                type="text" 
+                placeholder="Digite o título da tarefa" 
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                />
+            <input 
+                className="border-2 border-slate-300 outline-slate-400 px-4 py-2 rounded-md" 
+                type="text" 
+                placeholder="Digite a descrição da tarefa" 
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+                />
+            <button 
+                className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium" 
+                onClick={() => {
+                    if (!title.trim() || !description.trim()) {
+                        return alert("Preencha o título e a descrição da tarefa.")
+                    }
+                    onAddTaskSubmit(title, description);
+                    setTitle("");
+                    setDescription("");
+                }}
+            >Adicionar</button>
         </div>
     )
 }
+
+export default AddTasks;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,39 @@
-import { useState } from "react";
+import { ChevronRightIcon, Trash } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function Tasks() {
-    const [message, setMessage] = useState("Hello Test")
+function Tasks(props) {
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(task) {
+        const query = new URLSearchParams();
+        query.set("title", task.title);
+        query.set("description", task.description);
+        navigate(`/task?${query.toString()}`);
+    }
     return (
         <div>
-            <h1 className="">{message}</h1>
-            <button onClick={() => {setMessage("Hi!")}}>Responder</button>
+                <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow-md">
+                    {props.tasks.map((task) => (
+                        <li key={task.id} className="flex gap-2">
+                            <button 
+                                className={`bg-slate-400 text-white text-left p-2 w-full rounded-md ${task.isCompleted && 'line-through'}`}
+                                onClick={() => props.onTaskClick(task.id)}
+                            >
+                                {task.title}
+                            </button>
+                            <button 
+                                onClick={() => onSeeDetailsClick(task)}
+                                className="bg-slate-400 text-white p-2 rounded-md">
+                                <ChevronRightIcon />
+                            </button>
+                            <button 
+                                className="bg-slate-400 text-white p-2 rounded-md" 
+                                onClick={() => props.onDeleteTaskClick(task.id)}>
+                                <Trash />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
         </div>
     )
 }
